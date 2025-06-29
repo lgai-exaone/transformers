c080b30db11f5a7ce4b03acdc5e98f6366d8df2d
LG-AI-EXAONE <exaonemodels@lgresearch.ai>
2025-06-30 03:13:09 +0900

    Add license notice & Update modeling code

--- src/transformers/models/exaone4/modeling_exaone4.py
+++ src/transformers/models/exaone4/modeling_exaone4.py
@@ -407,6 +407,7 @@ class Exaone4PreTrainedModel(PreTrainedModel):
     supports_gradient_checkpointing = True
     _no_split_modules = ["Exaone4DecoderLayer"]
     _skip_keys_device_placement = ["past_key_values"]
+    _supports_flash_attn_3 = True
     _supports_flash_attn_2 = True
     _supports_sdpa = True
     _supports_flex_attn = True
